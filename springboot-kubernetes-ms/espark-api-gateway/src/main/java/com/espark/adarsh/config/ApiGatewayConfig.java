1c0cbda4963cbba0a3ed9a928cf1d4e289a217b0
adarshkumarsingh83@gmail.com <adarshkumarsingh83@gmail.com>
2020-05-15 08:46:47 -0500

    not working code

--- springboot-kubernetes-ms/espark-api-gateway/src/main/java/com/espark/adarsh/config/ApiGatewayConfig.java
+++ springboot-kubernetes-ms/espark-api-gateway/src/main/java/com/espark/adarsh/config/ApiGatewayConfig.java
@@ -14,7 +14,7 @@ public class ApiGatewayConfig {
                 .route(r -> r.path("/api/**")
                         .filters(f -> f.addRequestHeader("api-request", "api-request-header")
                                 .addResponseHeader("api-response", "api-response-header"))
-                        .uri("lb://espark-api-service")
+                        .uri("espark-api-service")
                         .id("espark-api-service"))
                 .build();
     }
