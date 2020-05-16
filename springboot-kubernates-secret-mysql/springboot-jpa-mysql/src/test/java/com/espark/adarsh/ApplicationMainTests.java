647eafcef5a75f47976aed51604a3504288e7d83
adarsh <us-guest@us-guests-MacBook-Pro.local>
2020-05-16 16:32:39 -0500

    adding non wokring

--- springboot-kubernates-secret-mysql/springboot-jpa-mysql/src/test/java/com/espark/adarsh/ApplicationMainTests.java
+++ springboot-kubernates-secret-mysql/springboot-jpa-mysql/src/test/java/com/espark/adarsh/ApplicationMainTests.java
@@ -1,12 +1,7 @@
 package com.espark.adarsh;
 
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@Slf4j
-@RunWith(SpringRunner.class)
 public class ApplicationMainTests {
 
 
