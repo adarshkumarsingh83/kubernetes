801803396f3092be878144b210fee52130572908
Kumar <adarshkumarsingh83@gmail.com>
2020-07-07 17:28:20 -0500

    working zuul with k8 registry and ms

--- springboot-kubernetes-cloud-ms/espark-zuul-gateway/src/main/java/com/espark/adarsh/EsparkZuulApplication.java
+++ springboot-kubernetes-cloud-ms/espark-zuul-gateway/src/main/java/com/espark/adarsh/EsparkZuulApplication.java
@@ -1,13 +1,19 @@
 package com.espark.adarsh;
 
+import com.espark.adarsh.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@AutoConfigureAfter(RibbonAutoConfiguration.class)
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class EsparkZuulApplication {
 
 	public static void main(String[] args) {
